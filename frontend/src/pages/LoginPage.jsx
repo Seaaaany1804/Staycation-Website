aed3df6e5eb6b375fd4a245cecb2c1e9a92e355b
TerenzTuason <terenzjetcemtuason@gmail.com>
2024-05-26 21:47:09 +0800

    updated ui

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import { AiOutlineMail, AiOutlineLock} from "react-icons/ai";
 import { Link } from 'react-router-dom'
 const LoginPage = () => {
   return (
-    <div className='bg-[#FFEEDB] h-[920px]'>
+    <div className='bg-[#FFEEDB] h-screen'>
         <div className='z-0'>
             <img src="../src/assets/trees logo.png" alt="error" className='fixed opacity-20 bottom-[-11%] left-[-7%] h-[700px]'/>
         </div>
@@ -12,7 +12,7 @@ const LoginPage = () => {
                 <h1 className='text-center mt-16 font-poppins text-[48px]'>Trees Residences</h1>
                 <h1 className='font-poppins mt-[10%] text-center text-3xl'>L O G I N</h1>
                 <div className=''>
-                    <forms className='grid place-items-center gap-5 mt-16 '>
+                    <forms className='grid place-items-center gap-5 mt-16 mx-[10%]'>
                         <div className='grid w-[60%] gap-1'>
                             <AiOutlineMail className='absolute mt-9 mx-2 text-[25px]'/>
                             <label className='font-poppins'>Email Address</label>
@@ -32,8 +32,8 @@ const LoginPage = () => {
                     </forms>
                 </div>
             </div>
-            <div className='flex justify-center items-center mt-16 mx-16'>
-                <img src="./src/assets/LoginImage.png" alt="error" className='rounded'/>
+            <div className='flex justify-center items-center mt-16 mr-16'>
+                <img src="./src/assets/LoginImage.png" alt="error" className='rounded h-[800px]'/>
             </div>
         </div>
     </div>
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -4,29 +4,82 @@ import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
   return (
-    <div className="bg-[#FFEEDB] h-[920px]">
-
+    <div className="bg-[#FFEEDB] h-screen">
+      <div className='z-0'>
+            <img src="../src/assets/trees logo.png" alt="error" className='fixed opacity-20 bottom-[-11%] left-[-7%] h-[700px]'/>
+        </div>
       <div className="grid grid-cols-2 absolute">
         <div>
           <h1 className="text-center mt-16 font-poppins text-[48px]">Trees Residences</h1>
-          <h1 className="font-poppins mt-[10%] text-center text-3xl">R E G I S T E R</h1>
+          <h1 className="font-poppins text-center text-3xl mt-10">R E G I S T E R</h1>
 
-          <form>
-            <div className="grid grid-cols-2 gap-5">
+          <form className="flex flex-col gap-5 mt-10">
+            <div className="grid grid-cols-2 gap-[10%] mx-[10%]">
                 <div className="grid">
                     <label>First Name</label>
-                    <input type="text" className="w-[50%]"/>
+                    <input type="text" className="w-[100%] p-5 h-[30px] rounded"/>
                 </div>
 
                 <div className="grid">
                     <label>Last Name </label>
-                    <input type="text" className="w-[50%]"/>
+                    <input type="text" className="w-[100%] h-[30px] p-5 rounded"/>
                 </div>
+            </div>
+            <div className="grid grid-cols-2 gap-[10%] mx-[10%]">
+                <div className="grid">
+                    <label>Middle Name</label>
+                    <input type="text" className="w-[100%] h-[30px] p-5 rounded"/>
+                </div>
+
+                <div className="grid">
+                    <label>Suffix</label>
+                    <input type="text" className="w-[100%] h-[30px] p-5 rounded"/>
+                </div>
+            </div>
+            <div className="grid grid-cols-2 gap-[10%] mx-[10%]">
+                <div className="grid">
+                    <label>Contact Number</label>
+                    <input type="text" className="w-[100%] h-[30px] p-5 rounded"/>
+                </div>
+
+                <div className="grid">
+                    <label>Gender</label>
+                    <input type="text" className="w-[100%] h-[30px] p-5 rounded"/>
+                </div>
+            </div>
+            <div className="grid grid-cols-2 gap-[10%] mx-[10%]">
+                <div className="grid">
+                    <label>Birthdate</label>
+                    <input type="text" className="w-[100%] h-[30px] p-5 rounded"/>
+                </div>
+            </div>
+            <div className="grid gap-[10%] mx-[10%]">
+                <div className="grid">
+                    <label>Email Address</label>
+                    <input type="text" className="w-[100%] h-[30px] p-5 rounded"/>
+                </div>
+            </div>
+            <div className="grid gap-[10%] mx-[10%]">
+                <div className="grid">
+                    <label>Password</label>
+                    <input type="text" className="w-[100%] h-[30px] p-5 rounded"/>
+                </div>
+            </div>
+            <div className="grid gap-[10%] mx-[10%]">
+                <div className="grid">
+                    <label>Re-enter Password</label>
+                    <input type="text" className="w-[100%] h-[30px] p-5 rounded"/>
+                </div>
+            </div>
+
+            <div className=" flex flex-col items-center gap-5">
+                <button type='submit' className='border-2 border-[#FF846D] bg-[#FF846D] rounded-full p-2 w-[40%] font-bold'>ENTER</button>
+                <h1>Already have an account? <Link to='/' className='underline font-bold'>Login now!</Link></h1>
             </div>
           </form>
         </div>
 
-        <div className='flex justify-center items-center mt-16 mx-16'>
+        <div className='flex justify-center items-center mt-16 mr-16'>
             <img src="./src/assets/RegisterImage.png" alt="error" className='rounded h-[800px]'/>
         </div>
       </div>
